3d5414ce9a27111085ac61b31fc1e172bc4b145d
BrendanMayer <brendan.mayer1234@gmail.com>
2025-08-23 22:07:07 +0100

    s3, dynamodb, sqs+dlq

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ds-assignment-2
+
+### Commit - 1
+I set up a cdk project and added an S3 bucket for photos, a DynamoDB table for images
+
+### Commit - 2
+I created an SQS queue with a DLQ, and hooked up S3 events to the queue.
--- lib/ds-assignment-2-stack.ts
+++ lib/ds-assignment-2-stack.ts
@@ -1,24 +1,52 @@
-import * as path from "path";
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
-import * as s3 from "aws-cdk-lib/aws-s3";
-import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
-export class DsAssignment2Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-    const bucket = new s3.Bucket(this, "PhotosBucket", {
+import * as cdk from 'aws-cdk-lib'
+import { Stack, StackProps, RemovalPolicy, Duration, CfnOutput } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+import * as sqs from 'aws-cdk-lib/aws-sqs'
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications'
+import * as iam from 'aws-cdk-lib/aws-iam'
+
+export class DsAssignment2Stack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
+
+    const bucket = new s3.Bucket(this, 'PhotosBucket', {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true
-    });
-    const table = new dynamodb.Table(this, "ImagesTable", {
-      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      encryption: s3.BucketEncryption.S3_MANAGED
+    })
+
+    const table = new dynamodb.Table(this, 'ImagesTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      stream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES
-    });
-    new cdk.CfnOutput(this, "BucketName", { value: bucket.bucketName });
-    new cdk.CfnOutput(this, "TableName", { value: table.tableName });
+      removalPolicy: RemovalPolicy.DESTROY
+    })
+
+    const dlq = new sqs.Queue(this, 'InvalidUploadsDLQ', {
+      retentionPeriod: Duration.days(4)
+    })
+
+    const uploadsQueue = new sqs.Queue(this, 'UploadsQueue', {
+      visibilityTimeout: Duration.seconds(60),
+      deadLetterQueue: { maxReceiveCount: 1, queue: dlq }
+    })
+
+    bucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3n.SqsDestination(uploadsQueue))
+
+    uploadsQueue.addToResourcePolicy(new iam.PolicyStatement({
+      principals: [new iam.ServicePrincipal('s3.amazonaws.com')],
+      actions: ['sqs:SendMessage'],
+      resources: [uploadsQueue.queueArn],
+      conditions: {
+        ArnEquals: { 'aws:SourceArn': bucket.bucketArn }
+      }
+    }))
+
+    new CfnOutput(this, 'BucketName', { value: bucket.bucketName })
+    new CfnOutput(this, 'TableName', { value: table.tableName })
+    new CfnOutput(this, 'UploadsQueueUrl', { value: uploadsQueue.queueUrl })
+    new CfnOutput(this, 'InvalidUploadsDLQUrl', { value: dlq.queueUrl })
   }
 }
